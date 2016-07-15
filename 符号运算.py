80738c75c2afd23e815800ce79fe03ad9d234b55
sunqi <156689004@qq.com>
2016-07-15 15:07:05 +0800

    pwd

--- 符号运算.py
+++ 符号运算.py
@@ -7,8 +7,9 @@ for j in range(60000):
     l1 = 1/(1+np.exp(-(np.dot(X,syn0)))) #输出b 
     l2 = 1/(1+np.exp(-(np.dot(l1,syn1)))) #输出y
     l2_delta = (y - l2)*(l2*(1-l2)) #g行向量
-    if j==1:print(l2_delta,syn1.T)
+    
     l1_delta = l2_delta.dot(syn1.T) * (l1 * (1-l1))
-    if j==1:print(l1_delta)
+    
     syn1 += l1.T.dot(l2_delta)
+    if j==1:print(l1,'\n',l2_delta)
     syn0 += X.T.dot(l1_delta)
